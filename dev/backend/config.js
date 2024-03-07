1527315d7c60cce43bd53476049ea5f9287f9adc
denisGolicin <den.golicin65@gmail.com>
2024-03-07 15:43:52 +0200

    init

--- dev/backend/config.js
+++
@@ -1,3 +0,0 @@
-export const PORT = process.env.PORT || 5000;
-export const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:5000/database';
-export const JWT_SECRET = process.env.JWT_SECRET || 'key';
